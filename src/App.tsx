62f04bb7225850806d1f2537959061c2d40fe202
shivakarka <shivakarka@gmail.com>
2023-01-11 21:47:02 +0530

    implemented InputForm functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import InputForm from "./components/InputForm";
@@ -16,13 +16,42 @@ function App() {
   const [list, setList] = useState<Ilist[]>([]);
   const [completedList, setCompletedList] = useState<Ilist[]>([]);
   const [editing, setEditing] = useState<boolean>(false);
-  const [editId, setEditId] = useState<number | null>(null);
+  const [editId, setEditId] = useState<number>(-1);
+
+  const handleSubmit = useCallback(
+    (todoName: string) => {
+      if (!todoName) {
+        alert("Enter a todo name");
+        return;
+      }
+      if (todoName && editing) {
+        const newList = list.filter((item) => item.id !== editId);
+        const newTodo = { id: editId, name: todoName, completed: false };
+        setList([...newList, newTodo]);
+        setEditing(false);
+        setEditId(-1);
+      } else {
+        const newTodo = {
+          id: list.length + 1,
+          name: todoName,
+          completed: false,
+        };
+        setList([...list, newTodo]);
+      }
+      setTodoName("");
+    },
+    [todoName, list, editing, editId]
+  );
 
   return (
     <div className="App">
       <h1>
         Todo App
-        <InputForm />
+        <InputForm
+          todoName={todoName}
+          editing={editing}
+          handleSubmit={handleSubmit}
+        />
         <TodoList />
         <CompletedList />
       </h1>
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function InputForm() {
-  return <div>InputForm</div>;
+interface IInputFormProps {
+  todoName: string;
+  handleSubmit: (todo: string) => void;
+  editing: boolean;
+}
+
+export default function InputForm({
+  todoName,
+  handleSubmit,
+  editing,
+}: IInputFormProps) {
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    if (editing) {
+      setTodo(todoName);
+    }
+  }, [editing]);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit(todo);
+        setTodo("");
+      }}
+    >
+      <input
+        type="text"
+        value={todo || ""}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">{editing ? "Edit" : "Add"}</button>
+    </form>
+  );
 }
